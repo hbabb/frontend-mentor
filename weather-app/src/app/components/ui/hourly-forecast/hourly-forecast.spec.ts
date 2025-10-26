44dd91ee51a80f065d7f605274ad54d1a3ea0c2d
Heath Babb <hbabb@heath-babb.dev>
2025-10-26 12:20:37 -0400

    initial commit for weather-app setup

---
+++ weather-app/src/app/components/ui/hourly-forecast/hourly-forecast.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HourlyForecast } from './hourly-forecast';
+
+describe('HourlyForecast', () => {
+  let component: HourlyForecast;
+  let fixture: ComponentFixture<HourlyForecast>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HourlyForecast]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HourlyForecast);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
